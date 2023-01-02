9d59fd5d06b152866c87e4b6efc7439b21de141c
masbagaspn <masbagaspn@gmail.com>
2023-01-03 02:43:16 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import './App.css'
+import { useEffect, useState, useRef } from 'react'
+
+function App() {
+  const [text, setText] = useState('Hello Diana')
+  let width = 100
+
+  const textRef = useRef(null)
+  const flowerRef = useRef(null)
+  
+  const interval = () => {
+    const textStyle = textRef.current.style
+    const flowerStyle = flowerRef.current.style
+
+    setTimeout(() => {
+      textStyle.transition = '1s opacity ease-in-out'
+      textStyle.opacity = '0'
+      setTimeout(() => {
+        setText(`Here's your flower`)
+        textStyle.opacity = '1'
+
+        setTimeout(() => {
+          textStyle.opacity = '0'
+
+          setTimeout(() => {
+            textStyle.display = 'none'
+            flowerStyle.opacity = '0'
+            flowerStyle.display = 'block'
+
+            setTimeout(() => {
+              flowerStyle.opacity = '1'
+              flowerStyle.transition = '1s opacity ease-in-out'        
+            }, 1000)
+          }, 1000)
+
+        }, 1500)
+
+      },1000)
+
+    }, 1500)
+  }
+
+  useEffect(() => {
+    interval()
+  }, [])
+  console.log(flowerRef)
+  return (
+    <div className="App">
+      <h1 ref={textRef}>{text}</h1>
+      <img className='flower' ref={flowerRef} src='../images/flowers-01.png' />
+    </div>
+  )
+}
+
+export default App
